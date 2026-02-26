451fd386dd411349fc8349d0cd9305bf18916e06
BYUN Sangpil <sangpire@gmail.com>
2026-02-26 13:12:24 +0900

    feat(app): 보안 컨텍스트 조건 분기 추가

--- app.js
+++ app.js
@@ -93,6 +93,13 @@ function registerServiceWorker() {
   if (!("serviceWorker" in navigator)) {
     return;
   }
+
+  const isLocalhost =
+    location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+  if (location.protocol !== "https:" && !isLocalhost) {
+    return;
+  }
 }
 
 startButton.addEventListener("click", startCamera);
