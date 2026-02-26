b26b72d13e173340abdda04a9ed07c9ee2f3e674
BYUN Sangpil <sangpire@gmail.com>
2026-02-26 13:11:08 +0900

    feat(sw): activate 이벤트 핸들러 추가

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(Promise.resolve());
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(Promise.resolve());
+});
