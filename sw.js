38255d4cad95ebbd97833bead0dcc5769e338c47
BYUN Sangpil <sangpire@gmail.com>
2026-02-26 13:11:49 +0900

    feat(sw): activate 단계에서 구버전 캐시 정리

--- sw.js
+++ sw.js
@@ -16,7 +16,15 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(Promise.resolve());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
