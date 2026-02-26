a01903ac4255e8a78a9c9cbad8696a979cab7777
BYUN Sangpil <sangpire@gmail.com>
2026-02-26 13:12:02 +0900

    feat(sw): fetch 단계에 캐시 전략 적용

--- sw.js
+++ sw.js
@@ -28,5 +28,28 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        const requestUrl = new URL(event.request.url);
+
+        if (requestUrl.origin === self.location.origin) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+
+        return networkResponse;
+      });
+    })
+  );
 });
